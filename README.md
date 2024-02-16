f3f8395d549e9edc08ca4d2503ea9aa57d148906
Huntermamunkingmamun <160240410+Huntermamunkingmamun@users.noreply.github.com>
2024-02-16 18:56:43 +0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,115 @@
+
+<h2 align="center">
+<a href="https://mao2116.xyz">TBOMB_MAO</a>
+  
+  </h2>
+</br>
+<b><i>THIS IS A SMS,CALL AND MAIL BOMBING TOOL,.
+IT 100% WORKING NOW,! YOU CAN SEND BULK SMS AND CALL UNLIMITED,!
+YOU CAN ALSO SEND MAIL,! WITH THIS TOOL.!</i></b>
+</br>
+</br>
+
+<p align="center">
+<b> AUTHER </b>
+</p>
+ <p align="center">
+<a href="https://github.com/mao2116">
+  <img width="50px" height="50px" src="https://raw.githubusercontent.com/fh-rabbi/Hack-Box/main/images/git.png">
+</a>
+<a href="https://www.facebook.com/mao2116/">
+  <img width="50px" height="50px" src="https://raw.githubusercontent.com/fh-rabbi/Hack-Box/main/images/fb.png"><!I JUST USE A PIC FROM FH-RABBI >
+</a></p>
+
+  
+#### THIS TOOL IS OPEN SOURCE FOR ONLY LEARNING PATH NOT FOR COPY.
+
+##### JUST ENTER YOUR COUNTRY CODE !
+
+
+
+
+
+### INSTALLATION [ RECOMMENDED ] :
+
+* `apt-get update -y`
+* `apt-get upgrade -y`
+* `apt install python git -y`
+* `pip install requests`
+* `git clone --depth=1 https://github.com/mao2116/tbomb_mao`
+### ONE CLICK INSTALLATION :
+
+
+* `apt-get update -y ; apt-get upgrade -y ; apt install python git -y ; pip install requests ; git clone --depth=1 https://github.com/mao2116/tbomb_mao`
+
+
+
+
+### FOR RUN THIS TOOL :
+
+***OPEN TERMINAL AND TYPE:***
+
+* `cd tbomb_mao`
+
+* `python tbomb_mao.py`
+
+
+<p align="left">
+  <a href="https://shell.cloud.google.com/cloudshell/open?cloudshell_git_repo=https://github.com/mao2116/tbomb_mao.git&tutorial=README.md" target="_blank"><img src="https://gstatic.com/cloudssh/images/open-btn.svg"></a>
+</p>
+
+
+
+***SMS BOMBING***
+
+* `python tbomb.py --bombing sms --cc <COUNTRY CODE> --number <NUMBER> --threat <BIMBING LIMIT>`
+
+***SMS AND CALL BOMBING***
+
+* `python tbomb.py --bombing sms_call --cc <COUNTRY CODE> --number <NUMBER> --threat <BIMBING LIMIT>`
+
+***EXAMPLE IN BD***
+
+* `python tbomb.py --bombing sms_call --cc 880 --number 01000000000 --threat 1000`
+
+## FOR HELP
+* `python tbomb.py -h`
+
+
+
+### FOR UPDATE TOOL ():
+
+* `cd ; rm -rf tbomb_mao ; apt-get update -y ; apt-get upgrade -y ; apt install python3 git -y ; pip install requests ; git clone --depth=1 https://github.com/mao2116/tbomb_mao`
+
+
+
+### ONE CLICK INSTALLATION (OLD AND SLOW):
+* `apt-get update -y && pkg install python -y && pip install requests && pkg install git -y && git clone https://github.com/mao2116/tbomb_mao && cd tbomb_mao`
+
+
+
+
+<h3>[ ! ] TASTED ON TERMUX [ ! ]<h3/>
+<h4>YOU CAN USE IT ANY KIND OF TERMINAL<h4/>
+
+
+
+<b>
+
+</br>
+</br>
+<p align="center">
+<a href="https://www.facebook.com/mao2116/">
+  <img width="50px" height="50px" src="https://raw.githubusercontent.com/fh-rabbi/Hack-Box/main/images/fb.png"><!I JUST USE A PIC FROM FH-RABBI >
+<a/>
+<p/>  
+
+</b>
+<b> mao2116 </b>
+
+## WARNING : 
+***This tool is only for educational purpose. If you use this tool for other purposes except education we will not be responsible in such cases.***
+
+  
+  
+  
---
+++ tbomb_mao.py
@@ -0,0 +1,124 @@
+#coding=utf-8
+#coding=utf-8
+#!/bin/python3
+
+
+
+# ASSALAMUALAIKUM
+
+# THIS IS A BANGLADESHI PROGRAMMER
+##########################################################
+# DEV : MAO2116
+# Github : https://github.com/mao2116
+
+# IF YOU WANNA TAKE CREADITS FOR THIS TOOL,,,LOOK YOUR SELF AGAIN AND AGAIN,,
+
+
+# THANKS TO ALLAH
+# WELCOME HOME
+#####################৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳৳################
+
+
+
+import sys, os
+acl='\033[1;30m'
+rcl='\033[1;31m'
+ycl='\033[1;33m'
+ncl='\033[0;00m'
+maoversys=sys.version_info[0]==3
+if maoversys:
+  pass
+else:
+  exit("\n{acl}[ {rcl}! {acl}]{ycl} USE PYTHON 3.6+ {acl}[ {rcl}!{acl} ]{ncl}\n".format(acl=acl,rcl=rcl,ycl=ycl,ncl=ncl))
+try:
+  import os,time
+except Exception as emao:
+  exit("\n{acl}[ {rcl}FILE ERROR TYPE : {acl}]{ycl}  {emao} {acl}[ {rcl}!{acl} ]{ncl}\n".format(acl=acl,rcl=rcl,ycl=ycl,ncl=ncl,emao=emao))
+try:
+  from core.logos import *
+  from core.ccchk import *
+except Exception as emao:
+  exit("\n{acl}[ {rcl}FILE ERROR TYPE : {acl}]{ycl}  {emao} {acl}[ {rcl}!{acl} ]{ncl}\n".format(acl=acl,rcl=rcl,ycl=ycl,ncl=ncl,emao=emao))
+  
+manu="""
+{acl}[ {ycl}1. {acl}] {gcl}SMS_BOMBING
+{acl}[ {ycl}2. {acl}] {gcl}CALL AND SMS_BOMBING
+{acl}[ {ycl}3. {acl}] {gcl}EMAIL BOMBING
+{acl}[ {ycl}4. {acl}] {gcl}MAO_COMMUNITY (FB PAGE)
+{acl}[ {ycl}5. {acl}] {gcl}TG ( MAO2116 )
+{acl}[ {rcl}0. {acl}] {rcl}EXIT
+""".format(acl=acl,gcl=gcl,ncl=ncl,rcl=rcl,ycl=ycl)
+try:
+  while True:
+    
+    os.system("clear")
+    print(logomao)
+    
+    print(manu)
+    mao=input('{ccl} ====>>{ycl} '.format(ccl=ccl,ycl=ycl))
+    if mao=="1":
+      os.system('clear')
+      print(logomao)
+      cc=input('{acl}>{mcl}>{acl}> {gcl}ENTER COUNTRY CODE {acl}:{ycl}'.format(acl=acl,mcl=mcl,gcl=gcl,ycl=ycl))
+      if cc.startswith("+"):
+        cc=cc.replace("+","")
+      else:
+        pass
+  
+      number=input('{acl}>{mcl}>{acl}>{gcl} ENTER NUMBER {acl}:{ycl}'.format(acl=acl,mcl=mcl,gcl=gcl,ycl=ycl))
+      get_phone_info(cc,number)
+      threat=input('{acl}>{mcl}>{acl}>{gcl} ENTER BOMBING LIMIT{acl}({pcl}MAX{acl}={gcl}1000{acl}) {acl}:{ycl}'.format(acl=acl,gcl=gcl,pcl=pcl,ycl=ycl,mcl=mcl))
+      if int(threat) <=1000:
+          pass
+      else:
+          exit("{acl}[{rcl} !{acl} ] {rcl}BOMBING LIMIT 1000 {acl}[{rcl} !{acl} ]\n".format(acl=acl,rcl=rcl))
+      os.system('python tbomb.py --bombing sms --cc {cc} --number {number} --threat {threat}'.format(cc=cc,number=number,threat=threat))
+      
+      break
+    elif mao=="2":
+      os.system('clear')
+      print(logomao)
+      cc=input('{acl}>{mcl}>{acl}> {gcl}ENTER COUNTRY CODE {acl}:{ycl}'.format(acl=acl,mcl=mcl,gcl=gcl,ycl=ycl))
+      if cc.startswith("+"):
+        cc=cc.replace("+","")
+      else:
+        pass
+  
+      number=input('{acl}>{mcl}>{acl}>{gcl} ENTER NUMBER {acl}:{ycl}'.format(acl=acl,mcl=mcl,gcl=gcl,ycl=ycl))
+      get_phone_info(cc,number)
+      threat=input('{acl}>{mcl}>{acl}>{gcl} ENTER BOMBING LIMIT{acl}({pcl}MAX{acl}={gcl}1000{acl}) {acl}:{ycl}'.format(acl=acl,gcl=gcl,pcl=pcl,ycl=ycl,mcl=mcl))
+      if int(threat) <=1000:
+          pass
+      else:
+          exit("{acl}[{rcl} !{acl} ] {rcl}BOMBING LIMIT 1000 {acl}[{rcl} !{acl} ]\n".format(acl=acl,rcl=rcl))
+      os.system('python tbomb.py --bombing sms_call --cc {cc} --number {number} --threat {threat}'.format(cc=cc,number=number,threat=threat))
+      break
+    elif mao =="3":
+      os.system('clear')
+      print(logomao)
+      mails=input('{acl}>{mcl}>{acl}>{gcl} ENTER MAIL {acl}:{ycl}'.format(acl=acl,mcl=mcl,gcl=gcl,ycl=ycl))
+      threat=input('{acl}>{mcl}>{acl}>{gcl} ENTER BOMBING LIMIT{acl}({pcl}MAX{acl}={gcl}1000{acl}) {acl}:{ycl}'.format(acl=acl,gcl=gcl,pcl=pcl,ycl=ycl,mcl=mcl))
+      if int(threat) <=1000:
+          pass
+      else:
+          exit("{acl}[{rcl} !{acl} ] {rcl}BOMBING LIMIT 1000 {acl}[{rcl} !{acl} ]\n".format(acl=acl,rcl=rcl))
+            
+      os.system('python tbomb.py --bombing mail --mail {mail} --threat {threat}'.format(mail=mails,threat=threat))
+      break
+
+    elif mao =="4":
+      os.system('xdg-open https://www.facebook.com/mao2116/')
+      break
+    elif mao=="5":
+      os.system('xdg-open https://t.me/maoCommunity')
+      break
+    elif mao=="0":
+      exit('{acl}[{gcl} ! {acl}]{rcl} THANKS FOR USING THIS TOOL {acl}[ {gcl}!{acl} ]'.format(acl=acl,gcl=gcl,rcl=rcl))
+    else:
+      print('{acl}[{rcl} !{acl} ]{rcl} WRONG SELECTION {acl}[ {rcl}! {acl}]{ncl}'.format(acl=acl,rcl=rcl,ncl=ncl))
+      time.sleep(1)
+    
+except KeyboardInterrupt:
+  exit('\n{acl} [{rcl} !{acl} ]{rcl} ABORTING... {acl}[{rcl} !{acl} ]\n'.format(acl=acl,rcl=rcl))
+except Exception as emao:
+  exit("\n{acl}[ {rcl} ERROR TYPE : {acl}]{ycl}  {emao} {acl}[ {rcl}!{acl} ]{ncl}\n".format(acl=acl,rcl=rcl,ycl=ycl,ncl=ncl,emao=emao))
